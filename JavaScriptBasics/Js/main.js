b35037dc644dc8acd2aecf18f6828851dff3b6a1
kaitokaneko <prfm.ankkaichan787@gmail.com>
2019-03-18 23:50:31 +0900

    JsBasics #16

--- JavaScriptBasics/Js/main.js
+++ JavaScriptBasics/Js/main.js
@@ -1,30 +1,9 @@
 'use strict'
 
-const score = 60;
-const name = "taguchi";
+// for
 
-const signal ="pink";
-
-// if (signal === "red") {
-//   console.log("stop!");
-// } else if(signal === "yellow") {
-//   console.log("caution!");
-// } else if(signal === "blue"){
-//   console.log("go!");
-// }
-
-switch (signal) {
-  case "red":
-  console.log("stop!");
-  break;
-  case "yellow":
-  console.log("caution!");
-  break;
-  case "blue":
-  case "green":
-  console.log("go!");
-  break;
-  default:
-  console.log("wrong signal!");
-  break;
+for (let i = 1; i <= 10; i++) {
+  // console.log("hello");
+  // console.log(i + "hello");
+  console.log(`${i} hello`);
 }
