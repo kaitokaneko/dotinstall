3dcf69f9b9fe9f7a5a71a691f21aeee293798b45
kaitokaneko <prfm.ankkaichan787@gmail.com>
2019-03-18 22:36:34 +0900

    JsBasics #8

--- JavaScriptBasics/Js/main.js
+++ JavaScriptBasics/Js/main.js
@@ -1,12 +1,17 @@
 'use strict'
 
-// 定数 const
 // 変数 let (var)
 
-let price = 150;
-console.log(price * 140);
-console.log(price * 160);
+let price = 500;
 
-price = 170;
-console.log(price * 140);
-console.log(price * 160);
+price = price + 100;
+price+= 100; // 700
+
+price*= 2; //price = price * 2 //1400
+
+price = price + 1; // 1401
+price += 1; // 1402
+price++; // 1403
+price-- ; //price -= 1; //1402
+
+console.log(price);
