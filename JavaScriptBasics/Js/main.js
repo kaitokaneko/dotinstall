1f4727cfa94b57e91ff4c26f29ab8e47fd1f1899
kaitokaneko <prfm.ankkaichan787@gmail.com>
2019-03-18 22:43:36 +0900

    JsBasics #10

--- JavaScriptBasics/Js/main.js
+++ JavaScriptBasics/Js/main.js
@@ -1,17 +1,7 @@
 'use strict'
 
-// 変数 let (var)
-
-let price = 500;
-
-price = price + 100;
-price+= 100; // 700
-
-price*= 2; //price = price * 2 //1400
-
-price = price + 1; // 1401
-price += 1; // 1402
-price++; // 1403
-price-- ; //price -= 1; //1402
-
-console.log(price);
+console.log(typeof 'hello');
+console.log(typeof 5);
+console.log(typeof true);
+console.log(typeof null);
+console.log(typeof undefined);
