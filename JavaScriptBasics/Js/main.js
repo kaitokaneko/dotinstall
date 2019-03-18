663dbe55a8c5d3cf727375bba9fa3e74c97cc8bd
kaitokaneko <prfm.ankkaichan787@gmail.com>
2019-03-18 22:21:25 +0900

    JsBasics #6

--- JavaScriptBasics/Js/main.js
+++ JavaScriptBasics/Js/main.js
@@ -1,13 +1,11 @@
 'use strict';
 
+// console.log(10 + 3); // 13
+// console.log(10 - 3); // 7
+// console.log(10 * 3); // 30
+// console.log(10 / 3); // 3.3333...
+// console.log(10 % 3); // 1
+// console.log(10 ** 3); // 1000
 
-// console.log("Hello");
-// console.log("Hello");
-
-// console.log('it's me!');
-
-// console.log("it's me!");
-// console.log('it\'s me!');
-
-// console.log('hell\no wor\tld');
-console.log('hello' + 'world');
+console.log((10 + 2) * 3); // 36
+console.log(10 + 2 * 3); // 16
