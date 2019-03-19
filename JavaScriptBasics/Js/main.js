03307ad3c51b768d0990f71ce8b4c85e0e8347d2
kaitokaneko <prfm.ankkaichan787@gmail.com>
2019-03-19 11:01:13 +0900

    JsBasics #23

--- JavaScriptBasics/Js/main.js
+++ JavaScriptBasics/Js/main.js
@@ -6,10 +6,22 @@
 //   // console.log(a + b + c);
 //   return a + b + c;
 
-const sum = function( a, b, c ) {
-  // console.log(a + b + c);
-  return a + b + c;
-}
+// const sum = function( a, b, c ) {
+//   // console.log(a + b + c);
+//   return a + b + c;
 
-const total = sum(1, 2, 3) + sum(3, 4, 5);
-console.log(total);
+// const sum = ( a, b, c ) => {
+//   // console.log(a + b + c);
+//   return a + b + c;
+
+// const sum = ( a, b, c ) => a + b + c;
+//
+// const total = sum(1, 2, 3) + sum(3, 4, 5);
+// console.log(total);
+
+// const double = function(a) {
+//   return a* 2;
+// }
+
+const double = a => 2 * a;
+console.log(double(12));
