9ce382f143ea747ac733be70d729edf2206c0f8c
kaitokaneko <prfm.ankkaichan787@gmail.com>
2019-03-20 22:21:05 +0900

    JsObjects #5

--- JavaScriptBasics/Js/main.js
+++ JavaScriptBasics/Js/main.js
@@ -1,25 +1,23 @@
 'use strict'
 
 {
- //オブジェクト
+ // クラス
 
-// const player = ["taguchi",32];
+class Player {
+  constructor(name, score) { //メソッド
+    this.name = name;
+    this.score = score;
+  }
+}
 
-const player = {
-  name: "taguchi",
-  score: 32,
+// const taguchi = {
+//   name: "taguchi",
+//   score: 32,
+// };
+//
+// const fkoji = {
+//   name: "fkoji",
+//   score: 44,
+// };
 
-};
-
-// console.log(player.name);
-// console.log(player["name"]);
-
-// player.score = 100;
-// console.log(player);
-
-// player.email = "taguchi@gmail.com";
-// console.log(player);
-
-delete player.score;
-console.log(player);
 }
