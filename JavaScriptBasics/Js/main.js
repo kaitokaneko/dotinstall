c64cbfb01f1958c606e01d5c51a5b12867b42e5b
kaitokaneko <prfm.ankkaichan787@gmail.com>
2019-03-20 22:04:11 +0900

    JsObjects #2

--- JavaScriptBasics/Js/main.js
+++ JavaScriptBasics/Js/main.js
@@ -8,6 +8,12 @@
  // const score3 = 40;
 
  const scores = [80,90,40];
- console.log(scores);
+ // console.log(scores[1]); //90
 
+// scores[2] = 100;
+// console.log(scores);
+
+// scores = 1;
+
+console.log(scores.length); // 3
 }
