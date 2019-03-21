f5347c8cdc5f617fc9117d2eb9bcb5a68adfc818
kaitokaneko <prfm.ankkaichan787@gmail.com>
2019-03-21 11:17:25 +0900

    JsObjects #6

--- JavaScriptBasics/Js/main.js
+++ JavaScriptBasics/Js/main.js
@@ -8,7 +8,22 @@ class Player {
     this.name = name;
     this.score = score;
   }
+  showInfo() {
+    console.log(`name: ${this.name} score: ${this.score}`);
+  }
+
+  static showVersion() {
+    console.log(`Player class ver. 1.0`);
+  }
 }
+
+const taguchi = new Player("taguchi",32);
+const fkoji = new Player("fkoji",44);
+
+
+Player.showVersion();
+
+// console.log(taguchi.name);
 
 // const taguchi = {
 //   name: "taguchi",
