0f25e3ff2d19c2e8c2b99a331f7ef609b02b1bb8
kaitokaneko <prfm.ankkaichan787@gmail.com>
2019-03-21 22:31:23 +0900

    JsDOM #8

--- JavaScriptBasics/Js/main.js
+++ JavaScriptBasics/Js/main.js
@@ -1,7 +1,17 @@
 'use strict'
 
 {
-const div = document.querySelector("div");
+  const div = document.querySelector("div");
 
-div.className = "box border-pink";
+  // div.className = "box border-pink";
+
+  // div.classList.add("border-pink");
+//   div.classList.remove("box");
+
+// if (div.classList.contains("border-pink")) {
+//   div.classList.remove("border-pink");
+//   } else {
+//   div.classList.add("border-pink");
+//   }
+    div.classList.taggle("border-pink");
 }
