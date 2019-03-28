2bc1abaf0a428dc3c8b87353d490b935ad7227b4
vagrant <vagrant@localhost.localdomain>
2019-03-29 08:22:58 +0900

    php_db #4

--- php_db_lessons/index.php
+++ php_db_lessons/index.php
@@ -7,12 +7,17 @@ define('PDO_DSN', 'mysql:dbhost=localhost;dbname=' . DB_DATABASE);
 
 try {
   // connect
-  $db = new PDO(PDO_DNS, DB_USERNAME, DB_PASSWORD);
-  $db->setAtrribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $db = new PDO(PDO_DSN, DB_USERNAME, DB_PASSWORD);
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-  //
+  // insert
+  $db->exec("insert into users (name, score) value ('taguchi', 55)");
+  echo "user added!";
 
-} catch (PDOExeption $e) {
+  // disconnect
+  $db = null;
+
+} catch (PDOException $e) {
   echo $e->getMessage();
   exit;
 }
