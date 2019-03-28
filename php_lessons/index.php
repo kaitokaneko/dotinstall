0798c0dbef61b9a15e3055cb0c5e1764ae2e426e
vagrant <vagrant@localhost.localdomain>
2019-03-28 09:52:01 +0900

    php #21

--- php_lessons/index.php
+++ php_lessons/index.php
@@ -1,34 +1,34 @@
 <?php
 
-// 継承
+// アクセス権
+// - private : そのクラス内からのみアクセス可能
+// - protected : そのクラス+親子クラス内からのみアクセス可能
+// - public : どこからでもアクセス可能
 
 class User {
-  // property
-  public $name;
+  // public $name;
+  // private $name;
+  // protected $name;
+
+  protected $name;
 
   //constructor
   public function __construct($name) {
     $this -> name = $name;
   }
   //method
-    final  public function sayHi() {
+  public function sayHi() {
     echo "hi, i am $this->name!";
   }
 }
 
 class AdminUser extends User {
   public function sayHello() {
-    echo "hello from Admin!";
-  }
-  // override
-  public function sayHi() {
-    echo "[admin] hi, i am $this->name!";
+    echo "hello from $this->name";
   }
 }
 
 $tom = new User("Tom");
 $steve = new AdminUser("Steve");
-// echo $steve->name;
-$tom->sayHi();
-$steve->sayHi();
-// $steve->sayHello();
+// echo $tom->name;
+$steve->sayHello();
