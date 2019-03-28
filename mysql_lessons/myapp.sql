9f4de7f59e101013b9360c1c29a1470070feaf8a
vagrant <vagrant@localhost.localdomain>
2019-03-29 06:07:05 +0900

    mysql #34

--- mysql_lessons/myapp.sql
+++ mysql_lessons/myapp.sql
@@ -2,29 +2,13 @@ drop table if exists posts;
 create table posts (
   id int unsigned primary key auto_increment,
   title varchar(225),
-  body text
+  body text,
+  created datetime default current_timestamp,
+  updated datetime default current_timestamp on update current_timestamp
 );
-
-drop table if exists logs;
-create table logs (
-  id int unsigned primary key auto_increment,
-  msg varchar(225)
-);
-
-drop trigger if exists posts_insert_trigger;
-delimiter //
-create trigger posts_update_trigger after update on posts for each row
-  begin
-    insert into logs (msg) values ('post updated!');
-    insert into logs (msg) values (concat(old.title, '->' , new.title));
-      end;
-//
-  delimiter ;
 
 insert into posts (title,body) values('title1','body 1');
 insert into posts (title,body) values('title2','body 2');
 insert into posts (title,body) values('title3','body 3');
 
-update posts set title = 'title 2 updated' where id = 2;
 select * from posts;
-select * from logs;
