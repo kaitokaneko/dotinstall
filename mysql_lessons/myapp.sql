dce15d8bc763647284cb40401f3da267c61af2e1
vagrant <vagrant@localhost.localdomain>
2019-03-29 01:48:43 +0900

    mysql #18

--- mysql_lessons/myapp.sql
+++ mysql_lessons/myapp.sql
@@ -3,14 +3,16 @@ create table users (
   id int unsigned primary key auto_increment,
   name varchar(20),
   score float,
-  rank enum('gold', 'silver', 'bronze')
+  coins set('gold', 'silver', 'bronze')
 );
 
-insert into users (name, score, rank) values ('taguchi', 5.8, 'silver');
-insert into users (name, score, rank) values ('fkoji', 8.2, 'gold');
-insert into users (name, score, rank) values ('dotinstall', 6.1, 'red');
+insert into users (name, score, coins) values ('taguchi', 5.8, 'gold,silver');
+insert into users (name, score, coins) values ('fkoji', 8.2, 'bronze,gold');
+insert into users (name, score, coins) values ('dotinstall', 6.1, 'red');
 
 
 -- select * from users;
-select * from users where rank = 'silver';
-select * from users where rank = 2;
+-- select * from users where coins = 'gold,silver';
+-- select * from users where coins like '%gold%';
+
+select * from users where coins = 3;
