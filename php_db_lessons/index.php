4fb5cbf479c372a1afb893e97dbc46dd2eaa145c
vagrant <vagrant@localhost.localdomain>
2019-03-29 08:49:40 +0900

    php_db #6

--- php_db_lessons/index.php
+++ php_db_lessons/index.php
@@ -15,9 +15,12 @@ try {
   (2) query(): 結果を返す、安全、何回も実行されないSQL
   (3) prepare(): 結果を返す、安全対策が必要、複数回実行されるSQL
   */
+  // insert
 
-  $stmt = $db->prepare("insert into users (name, score) values (?, ?)");
-  $stmt->execute(['taguchi', 44]);
+  // $stmt = $db->prepare("insert into users (name, score) values (?, ?)");
+  // $stmt->execute(['taguchi', 44]);
+  $stmt = $db->prepare("insert into users (name, score) values (:name, :score)");
+  $stmt->execute([':name'=>'fkoji', ':score' => 80]);
   echo "inserted: ". $db->lastInsertID();
 
 } catch (PDOException $e) {
