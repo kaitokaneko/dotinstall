4d73e65155ab9bb2635fe22614a17ed1b1bfd580
vagrant <vagrant@localhost.localdomain>
2019-03-29 05:58:56 +0900

    mysql #33

--- mysql_lessons/myapp.sql
+++ mysql_lessons/myapp.sql
@@ -12,15 +12,19 @@ create table logs (
 );
 
 drop trigger if exists posts_insert_trigger;
-create trigger posts_insert_trigger after insert on posts for each row insert into
-logs (msg) values ('post added!');
+delimiter //
+create trigger posts_update_trigger after update on posts for each row
+  begin
+    insert into logs (msg) values ('post updated!');
+    insert into logs (msg) values (concat(old.title, '->' , new.title));
+      end;
+//
+  delimiter ;
 
 insert into posts (title,body) values('title1','body 1');
 insert into posts (title,body) values('title2','body 2');
 insert into posts (title,body) values('title3','body 3');
 
+update posts set title = 'title 2 updated' where id = 2;
 select * from posts;
 select * from logs;
-
--- show triggers;
-show triggers \G;
