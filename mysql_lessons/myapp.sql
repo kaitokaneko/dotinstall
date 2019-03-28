38169316f3141c838d0943f0b495d1a38d77b909
vagrant <vagrant@localhost.localdomain>
2019-03-29 05:09:21 +0900

    mysql #27

--- mysql_lessons/myapp.sql
+++ mysql_lessons/myapp.sql
@@ -1,14 +1,25 @@
-drop table if exists users;
-create table users (
+drop table if exists posts;
+create table posts (
   id int unsigned primary key auto_increment,
-  name varchar(20),
-  score float
+  title varchar(225),
+  body text
 );
 
-alter table users add index index_score (score);
--- show index from users;
--- explain select * from users where score > 5.0;
--- explain select * from users where name > 'taguchi';
+drop table if exists comments;
+create table comments (
+  id int unsigned primary key auto_increment,
+  post_id int not null,
+  body text
+);
 
-alter table users drop index index_score;
-show index from users;
+insert into posts (title,body) values('title1','body 1');
+insert into posts (title,body) values('title2','body 2');
+insert into posts (title,body) values('title3','body 3');
+
+insert into comments (post_id,body) values(1,'first comment for post 1');
+insert into comments (post_id,body) values(1,'second comment for post 1');
+insert into comments (post_id,body) values(3,'first comment for post 3');
+insert into comments (post_id,body) values(4,'first comment for post 4');
+
+select * from posts;
+select * from comments;
