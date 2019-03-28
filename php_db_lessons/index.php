02ab6ea8edc2eac275ea47ad63122f064208a35f
vagrant <vagrant@localhost.localdomain>
2019-03-29 08:08:11 +0900

    php_db #3

---
+++ php_db_lessons/index.php
@@ -0,0 +1,18 @@
+<?php
+
+define('DB_DATABASE', 'dotinstall_db');
+define('DB_USERNAME', 'dbuser');
+define('DB_PASSWORD', 'g6fd7Xx');
+define('PDO_DSN', 'mysql:dbhost=localhost;dbname=' . DB_DATABASE);
+
+try {
+  // connect
+  $db = new PDO(PDO_DNS, DB_USERNAME, DB_PASSWORD);
+  $db->setAtrribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  //
+
+} catch (PDOExeption $e) {
+  echo $e->getMessage();
+  exit;
+}
