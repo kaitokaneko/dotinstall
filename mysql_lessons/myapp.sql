45795a1fa8284e211d899beccee9142ec7fedc90
vagrant <vagrant@localhost.localdomain>
2019-03-29 01:31:44 +0900

    mysql #16

--- mysql_lessons/myapp.sql
+++ mysql_lessons/myapp.sql
@@ -12,14 +12,12 @@ insert into users (name, score) values ('Tanaka', 4.2);
 insert into users (name, score) values ('yamada', null);
 insert into users (name, score) values ('tashiro', 7.9);
 
---  + - * / %
--- update users set score = score * 1.2 where id % 2 = 0;
--- select * from users;
+-- select length ('hello'); -- 5
+-- select substr('hello', 2); -- ello
+-- select substr('hello', 2, 3); -- ell
+-- select upper('hello'); -- HELLO
+-- select lower('hello'); -- hello
+-- select concat('hello', 'world'); -- helloworld
 
--- select round(5.355); -- 5
--- select round(5.355, 1); -- 5.4
--- select floor(5.833); -- 5
--- select ceil(5.238); -- 6
--- select rand();
-
-select * from users order by rand() limit 1;
+select length(name), name from users order by length(name);
+select length(name) as len, name from users order by len;
