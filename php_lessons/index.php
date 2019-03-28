98188059f901605dfb2e1db890ee28651d5e8150
vagrant <vagrant@localhost.localdomain>
2019-03-28 12:54:50 +0900

    php #29

--- php_lessons/index.php
+++ php_lessons/index.php
@@ -1,26 +1,9 @@
 <?php
 
-$username = "";
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-  $username = $_POST["username"];
-  $err = false;
-  if (strlen($username) > 8) {
-   $err = true;
- }
-}
-?>
-<!DOCTYPE html>
-<html lang="ja">
-  <head>
-    <meta charset="utf-8">
-    <title>Check username</title>
-  </head>
-  <body>
-    <form action="" method="POST">
-      <input type="text" name="username" placeholder="user name"
-      value="<?php echo htmlspecialchars($username,ENT_QUOTES,"utf-8"); ?>">
-      <input type="submit" value="check">
-      <?php if ($err) { echo "Too long!"; } ?>
-    </form>
-  </body>
-</html>
+// Cookie
+
+// setcookie("username","taguchi");
+// setcookie("username","taguchi", time()+60*60);
+setcookie("username","taguchi", time()-60*60);
+
+echo $_COOKIE["username"];
