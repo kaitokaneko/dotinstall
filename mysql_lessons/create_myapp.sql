b96029d980be979af93c0097389f8bcd40eecd60
vagrant <vagrant@localhost.localdomain>
2019-03-28 17:39:34 +0900

    mysql #5

---
+++ mysql_lessons/create_myapp.sql
@@ -0,0 +1,12 @@
+# comment
+-- comment
+/*
+comment
+comment
+comment
+*/
+
+
+drop database if exists myapp;
+create database myapp;
+grant all on myapp.* to myapp_user@localhost identified by '2VNAh1go'
