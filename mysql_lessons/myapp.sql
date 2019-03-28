639c9923ab979f66b91d601cba45cae2a677f01f
vagrant <vagrant@localhost.localdomain>
2019-03-29 05:41:48 +0900

    mysql #31

--- mysql_lessons/myapp.sql
+++ mysql_lessons/myapp.sql
@@ -12,8 +12,6 @@ create table comments (
   body text
 );
 
-alter table comments add constraint fk_comments foreign key (post_id) references posts (id);
-
 insert into posts (title,body) values('title1','body 1');
 insert into posts (title,body) values('title2','body 2');
 insert into posts (title,body) values('title3','body 3');
@@ -21,7 +19,11 @@ insert into posts (title,body) values('title3','body 3');
 insert into comments (post_id,body) values(1,'first comment for post 1');
 insert into comments (post_id,body) values(1,'second comment for post 1');
 insert into comments (post_id,body) values(3,'first comment for post 3');
-insert into comments (post_id,body) values(4,'first comment for post 4');
+
+delete from posts where id = 2;
+  insert into posts (title,body) values('new title','new body');
+  -- insert into comments (post_id,body) values(4,'first comment new post 3');
+  insert into comments (post_id,body) values(last_insert_id(),'first comment for new post');
 
 select * from posts;
 select * from comments;
