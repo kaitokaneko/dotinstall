5b03ac5a88cd274fc247a240a7501a906d6f2fa1
vagrant <vagrant@localhost.localdomain>
2019-03-29 05:49:53 +0900

    mysql #32

--- mysql_lessons/myapp.sql
+++ mysql_lessons/myapp.sql
@@ -5,25 +5,22 @@ create table posts (
   body text
 );
 
-drop table if exists comments;
-create table comments (
+drop table if exists logs;
+create table logs (
   id int unsigned primary key auto_increment,
-  post_id int unsigned not null,
-  body text
+  msg varchar(225)
 );
+
+drop trigger if exists posts_insert_trigger;
+create trigger posts_insert_trigger after insert on posts for each row insert into
+logs (msg) values ('post added!');
 
 insert into posts (title,body) values('title1','body 1');
 insert into posts (title,body) values('title2','body 2');
 insert into posts (title,body) values('title3','body 3');
 
-insert into comments (post_id,body) values(1,'first comment for post 1');
-insert into comments (post_id,body) values(1,'second comment for post 1');
-insert into comments (post_id,body) values(3,'first comment for post 3');
-
-delete from posts where id = 2;
-  insert into posts (title,body) values('new title','new body');
-  -- insert into comments (post_id,body) values(4,'first comment new post 3');
-  insert into comments (post_id,body) values(last_insert_id(),'first comment for new post');
-
 select * from posts;
-select * from comments;
+select * from logs;
+
+-- show triggers;
+show triggers \G;
