c5c54d060f6811f8d21fb708bdd3da8bb57b5fc7
vagrant <vagrant@localhost.localdomain>
2019-03-29 02:10:52 +0900

    mysql #20

--- mysql_lessons/myapp.sql
+++ mysql_lessons/myapp.sql
@@ -1,4 +1,5 @@
 drop table if exists users;
+drop table if exists users_with_team;
 create table users (
   id int unsigned primary key auto_increment,
   name varchar(20),
@@ -12,32 +13,24 @@ insert into users (name, score) values ('Tanaka', 4.2);
 insert into users (name, score) values ('yamada', null);
 insert into users (name, score) values ('tashiro', 7.9);
 
+-- create table users_with_team as
 -- select
+--   id,
 --   name,
 --   score,
---   if (score > 5.0, 'OK', 'NG') as result
+--   case
+--     when score > 8.0 then 'Team-A'
+--     when score > 6.0 then 'Team-B'
+--     else 'Team-C'
+--     end as team
 --  from
 --   users;
+--
+--   select * from users_with_team;
 
+-- create table users_copy select * from users;
+-- select * from users_copy;
 
--- select
---   name,
---   score,
---   case floor(score) % 2
---     when 0 then 'even'
---     when 1 then 'odd'
---     else null
---     end as type
---  from
---   users;
-
-select
-  name,
-  score,
-  case
-    when score > 8.0 then 'Team-A'
-    when score > 6.0 then 'Team-B'
-    else 'Team-C'
-    end as team
- from
-  users;
+create table users_empty like users;
+desc users_empty;
+select * from users_empty;
