aebf99c0678f8c8cf49cba7ff5bff9555c0d9d9e
vagrant <vagrant@localhost.localdomain>
2019-03-28 12:12:16 +0900

    php #25

---
+++ php_lessons/User.class.php
@@ -0,0 +1,11 @@
+<?php
+
+Class User {
+  public $name;
+  public function __construct($name) {
+    $this->name = $name;
+  }
+  public function sayHi() {
+    echo "hi, i am $this->name!";
+  }
+}
