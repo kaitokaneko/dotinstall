5af49ad995a5f8ff534cb2c5efc49f43d1f79296
vagrant <vagrant@localhost.localdomain>
2019-03-28 21:17:22 +0900

    mysql #9

--- mysql_lessons/myapp.sql
+++ mysql_lessons/myapp.sql
@@ -1,19 +1,20 @@
 drop table if exists users;
 create table users (
-  id int unsigned,
-  name varchar(20),
-  score float
+  id int unsigned primary key auto_increment,
+  name varchar(20) unique,
+  -- score float not null
+  score float default 0.0
 );
+desc users;
+insert into users (name, score) values('taguchi', 5.8);
+insert into users (name, score) values('fkoji', 8.2);
+insert into users (name, score) values('dotinstall', 6.1);
 
 -- insert into users (id, name, score) values(1, 'taguchi', 5.8);
 -- insert into users (id, name, score) values(2, 'fkoji', 8.2);
 -- insert into users (id, name, score) values(3, 'dotinstall', 6.1);
--- insert into users (id, name, score) values(4, 'yamada', null);
-
-insert into users (id, name, score) values
-  (1, 'taguchi', 5.8),
-  (2, 'fkoji', 8.2),
-  (3, 'dotinstall', 6.1),
-  (4, 'yamada', null);
+-- insert into users (id, name, score) values(4, 'yamada',null);
+-- insert into users (id, name) values(5, 'tanaka');
+-- insert into users (id, name) values(6, 'tanaka');
 
 select * from users;
