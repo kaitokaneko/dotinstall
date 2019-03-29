9fd3b54b4a9307241f4ba7a602f0233c49d04639
vagrant <vagrant@localhost.localdomain>
2019-03-29 22:14:49 +0900

    todo_php #5

--- todo_app_php/index.php
+++ todo_app_php/index.php
@@ -9,23 +9,24 @@ require_once(__DIR__ . '/functions.php');
 <head>
   <mrta charset="utf-8">
   <title>My Todos</title>
+  <link rel="stylesheet" href="styles.css">
 </head>
 <body>
   <div id="container">
     <h1>Todos</h1>
     <form action="">
-      <input type="text" placeholder="What needs to be done?">
+      <input type="text" placeholder="What needs to be done?" id="new_todo">
     </form>
     <ul>
       <li>
       <input type="checkbox">
       <span>Do something</span>
-      <div>x</div>
+      <div class="delete_todo">x</div>
       </li>
       <li>
       <input type="checkbox" checked>
-      <span>Do something again</span>
-      <div>x</div>
+      <span class="none">Do something again</span>
+      <div class="delete_todo">x</div>
       </li>
     </ul>
   </div>
