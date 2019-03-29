e17f954b919f65332b0e943aa20788c6828e0bbd
vagrant <vagrant@localhost.localdomain>
2019-03-29 10:00:27 +0900

    php_db #12

--- php_db_lessons/index.php
+++ php_db_lessons/index.php
@@ -5,27 +5,25 @@ define('DB_USERNAME', 'dbuser');
 define('DB_PASSWORD', 'g6fd7Xx');
 define('PDO_DSN', 'mysql:dbhost=localhost;dbname=' . DB_DATABASE);
 
-class User {
-  public $id;
-  public $name;
-  public $score;
-  public function show() {
-    echo "$this->name ($this->score)";
-  }
-}
-
 try {
   // connect
   $db = new PDO(PDO_DSN, DB_USERNAME, DB_PASSWORD);
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-  // select
-  // FETCH_CLASS
-  $stmt = $db->query("select * from users");
-  $users = $stmt -> fetchAll(PDO::FETCH_CLASS, 'User');
-  foreach ($users as $user) {
-    $user->show();
-  }
+  // update
+  $stmt = $db->prepare("update users set score = :score where name = :name");
+  $stmt->execute([
+    ':score' => 100,
+    ':name' => 'taguchi'
+  ]);
+  echo 'row updated: ' . $stmt->rowCount();
+
+  // delete
+  $stmt = $db->prepare("delete from users where name = :name");
+  $stmt->execute([
+    ':name' => 'dotinstall'
+  ]);
+  echo 'row deleted: ' . $stmt->rowCount();
 
 } catch (PDOException $e) {
   echo $e->getMessage();
