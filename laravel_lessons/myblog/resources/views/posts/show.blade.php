71b71e9a18b3693e7a09b5c6fd40f7e33fb3b4dd
vagrant <vagrant@localhost.localdomain>
2019-03-29 20:16:52 +0900

    laravel_lessons #27

--- laravel_lessons/myblog/resources/views/posts/show.blade.php
+++ laravel_lessons/myblog/resources/views/posts/show.blade.php
@@ -7,4 +7,16 @@
   <a href="{{ url('/') }}" class="header-menu">Back</a>
   {{ $post -> title}}</h1>
 <p>{!! nl2br(e($post->body)) !!}</p>
+
+<h2>Comments</h2>
+<ul>
+@forelse ($post -> comments as $comment)
+<li>
+  {{$comment->body}}
+</li>
+@empty
+<li>No comments yet</li>
+@endforelse
+</ul>
+
 @endsection
