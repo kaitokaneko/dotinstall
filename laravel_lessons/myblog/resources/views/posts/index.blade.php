ba92d774b79fdf1f12749b622c033eaf6693ac41
vagrant <vagrant@localhost.localdomain>
2019-03-29 19:07:17 +0900

    laravel_lessons #21

--- laravel_lessons/myblog/resources/views/posts/index.blade.php
+++ laravel_lessons/myblog/resources/views/posts/index.blade.php
@@ -8,13 +8,16 @@ Blog Posts
 
 @section('content')
 <h1>
-<a href="{{ url('/posts/create') }}" class="header-menu">New Post</a>
-  Blog Posts</h1>
+  <a href="{{ url('/posts/create') }}" class="header-menu">New Post</a>
+  Blog Posts
+</h1>
+
 <ul>
-@forelse ($posts as $post)
-<li><a href="{{ action('PostsController@show', $post) }}">{{ $post->title }}</a></li>
-@empty
-<li>No posts yet</li>
-@endforelse
+  @forelse ($posts as $post)
+  <li><a href="{{ action('PostsController@show', $post) }}">{{ $post->title }}</a></li>
+  <li><a href="{{ action('PostsController@edit', $post) }}" class="edit">[edit]</a></li>
+  @empty
+  <li>No posts yet</li>
+  @endforelse
 </ul>
 @endsection
