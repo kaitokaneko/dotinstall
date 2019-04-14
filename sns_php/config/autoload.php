233396ad7cf0aeeb666950061b0c3bbdfb7ec865
vagrant <vagrant@localhost.localdomain>
2019-04-14 21:06:46 +0900

    sns_php #4

---
+++ sns_php/config/autoload.php
@@ -0,0 +1,8 @@
+<?php
+
+/*
+MyApp
+index.php controller
+MyApp\Controller\index
+-> lib/Controller/Index.php
+*/
---
+++ sns_php/config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+ini_set('display_error',1);
+define('DSN','mysql:dbhost=localhost;dbname=dotinstall_sns_php');
+define('DB_USERNAME', 'dbuser')
+define('DB_PASSWORD', 'mu4uJsif')
+
+require_once(__DIR__ . '/../lib/functions.php');
+require_once(__DIR__ . '/autoload.php');
+
+session_start();
