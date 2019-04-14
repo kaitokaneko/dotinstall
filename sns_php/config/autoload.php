a2235f3298eb17a708a7ca83364791c353de384c
vagrant <vagrant@localhost.localdomain>
2019-04-14 21:18:49 +0900

    sns_php #5

--- sns_php/config/autoload.php
+++ sns_php/config/autoload.php
@@ -6,3 +6,14 @@ index.php controller
 MyApp\Controller\index
 -> lib/Controller/Index.php
 */
+
+spl_autoload_register(function($class) {
+  $prefix = 'MyApp\\';
+  if (strpos($class, $prefix) === 0) {
+    $classname = substr($class, strlen($prefix));
+    $classFilePath = __DIR__ . '/../lib/' . str_replace('\\', '/', $className) . '.php';
+      if (file_exists($classFilePath)) {
+        require $classFilePath;
+      }
+  }
+});
