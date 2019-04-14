b2501ad2b54ad667973ba070acd8e717a999a839
vagrant <vagrant@localhost.localdomain>
2019-04-14 21:47:36 +0900

    sns_php #6

---
+++ sns_php/lib/Controller/Index.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace MyApp\Controller;
+
+class Index extends \MyApp\Controller {
+
+  public function run() {
+    if ($this->isLoggedin()) {
+      // login
+    }
+
+     // get return info
+  }
+}
---
+++ sns_php/public_html/index.php
@@ -0,0 +1,9 @@
+<?php
+
+// ユーザの一覧
+
+require_once(__DIR__ . '/../config/config.php');
+
+$app = new MyApp\Controller\Index();
+
+$app->run();
