6a4a25458eb7cce71bd6fb7c7f74cc2e002355da
vagrant <vagrant@localhost.localdomain>
2019-04-16 11:40:31 +0900

    sns_php #14

--- sns_php/lib/Controller.php
+++ sns_php/lib/Controller.php
@@ -4,6 +4,25 @@ namespace MyApp;
 
 class Controller {
 
+  private $_errors;
+
+  public function __construct() {
+    $this->_errors = new \stdClass();
+  }
+
+  protected function setErrors($key, $error) {
+    $this->_errors-> $key = $error;
+  }
+
+  public function getErrors($key) {
+    return isset($this->_error->$key) ? = $this->_errors->$key : '';
+  }
+
+  protected function hasError() {
+    return !empty(get_object_vars($this->_errors));
+  }
+
+
   protected function isLoggedIn() {
     // $_SESSION['me']
     return isset($_SESSION['me']) && !empty($_SESSION['me']);
