9d57c6e0c7a6d19b3e497f0569f48fc0da403fa2
vagrant <vagrant@localhost.localdomain>
2019-04-20 05:27:43 +0900

    sns_php #25

--- sns_php/public_html/index.php
+++ sns_php/public_html/index.php
@@ -9,6 +9,10 @@ require_once(__DIR__ . '/../config/config.php');
 $app = new MyApp\Controller\Index();
 
 $app->run();
+
+// $app->me()
+// $app->getValues()->users
+
 ?>
 
 <!DOCTYPE html>
@@ -21,14 +25,15 @@ $app->run();
 <body>
   <div id="container">
     <form action="logout.php" method="post" id="logout">
-      aaa@gmail.com <input type="submit" value="Log Out">
+      <?= h($app->me()->email); ?> <input type="submit" value="Log Out">
       <input type="hidden" name="token" value="<?= h($_SESSION['token']); ?>">
     </form>
-    <h1>Users <span class="fs12">(3)</span></h1>
+    <h1>Users <span class="fs12">(<?= count($app->getValues()->users);
+    ?>)</span></h1>
     <ul>
-      <li>aaa@gmail.com</li>
-      <li>aaa@gmail.com</li>
-      <li>aaa@gmail.com</li>
+      <?php foreach ($app->getValues()->users as $user) : ?>
+        <li><?= h($user->email); ?></li>
+      <?php endforeach; ?>
     </ul>
   </div>
 </body>
