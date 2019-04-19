85fbec587a262f65e6c5fe6ea12db730b25293bb
vagrant <vagrant@localhost.localdomain>
2019-04-20 00:34:48 +0900

    sns_php #17

--- sns_php/lib/Controller/Signup.php
+++ sns_php/lib/Controller/Signup.php
@@ -37,8 +37,19 @@ class Signup extends \MyApp\Controller {
       return;
     } else {
       // create user
-
+      try {
+        $userModel = new \MyApp\Model\User();
+        $userModel->create([
+          'email' => $_POST['email']
+          'password' => $_POST['password']
+        ]);
+      } catch (\MyApp\Exception\DuplicateEmail $e) {
+        $this->setErrors('email', $e->getMessege());
+        return
+      }
       // redirect to login
+      header('Location: ' . SITE_URL . '/login.php');
+      exit;
     }
   }
 
