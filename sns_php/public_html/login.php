e54caaf984df0150eee6669a8cfa392ee3472cfe
vagrant <vagrant@localhost.localdomain>
2019-04-20 04:19:25 +0900

    sns_php #20

--- sns_php/public_html/login.php
+++ sns_php/public_html/login.php
@@ -4,9 +4,9 @@
 
 require_once(__DIR__ . '/../config/config.php');
 
-// $app = new MyApp\Controller\Login();
-//
-// $app->run();
+$app = new MyApp\Controller\Login();
+
+$app->run();
 
 // echo "login screen";
 // exit;
